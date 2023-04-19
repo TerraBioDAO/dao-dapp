c409ec56539cfb8510e6a011e89282742c60f75d
raphael <raphael.pellet@protonmail.com>
2023-04-19 15:27:06 +0200

    creation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import { ChakraProvider } from "@chakra-ui/react"
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit"
+import { getDefaultProvider } from "ethers"
+import type { AppProps } from "next/app"
+import { WagmiConfig, configureChains, createClient } from "wagmi"
+import { MetaMaskConnector } from "@wagmi/core/connectors/metaMask"
+import { WalletConnectConnector } from "@wagmi/core/connectors/walletConnect"
+
+const client = createClient({
+  connectors: [
+    new MetaMaskConnector(),
+    // new WalletConnectConnector({
+    //   chains: undefined,
+    //   options: {} as WalletConnectOptions,
+    // }),
+  ],
+  provider: getDefaultProvider(),
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <WagmiConfig client={client}>
+        {/* <RainbowKitProvider chains={}> */}
+        <Component {...pageProps} />
+        {/* </RainbowKitProvider> */}
+      </WagmiConfig>
+    </ChakraProvider>
+  )
 }
